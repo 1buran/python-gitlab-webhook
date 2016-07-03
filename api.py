0a7734d94e4e50c5e37184708594145e492fff97
Andrew Burdyug <buran83@gmail.com>
2016-07-03 11:24:01 +0300

    Implement: run_on_push feature

--- api.py
+++ api.py
@@ -9,17 +9,17 @@ import os.path
 import re
 import subprocess
 import json
-import time
 import logging
 import logging.config
 import falcon
 import yaml
 import requests
 import tempfile
-from multiprocessing import Queue
+from multiprocessing import Queue, Process
 
 conf = {}
 merge_requests_queue = Queue()
+push_queue = Queue()
 
 with open('config.yml') as config_file:
     conf.update(yaml.load(config_file))
@@ -97,8 +97,8 @@ def run_cmd(cmd):
     """Run command in shell."""
     try:
         output = subprocess.check_output(
-            cmd, shell=True, stderr=subprocess.STDOUT,
-            universal_newlines=True)
+            cmd, executable='/bin/bash', shell=True,
+            stderr=subprocess.STDOUT, universal_newlines=True)
     except subprocess.CalledProcessError as er:
         return False, er.output
     else:
@@ -122,14 +122,18 @@ class GitLabWebHookReceiver:
                 'Missing thing',
                 'A thing must be submitted in the request body.')
         self.log.debug('received data: %s', payload, extra=req.env)
-        merge_requests_queue.put_nowait(payload)
+        if payload['object_kind'] == 'merge_request':
+            merge_requests_queue.put_nowait(payload)
+        elif payload['object_kind'] == 'push':
+            push_queue.put_nowait(payload)
         resp.status = falcon.HTTP_201
 
 
 class GitLabAPI:
     """Simple class for using GitLab API."""
 
-    def __init__(self, repo_url, clone_url, project_id, branch, merge_id):
+    def __init__(self, repo_url, clone_url, project_id, branch, action_type,
+                 merge_id=None):
         """Standart init method."""
         self.token = conf['gitlab_auth_token']
         self.session = None
@@ -137,6 +141,7 @@ class GitLabAPI:
         self.clone_url = clone_url
         self.project_id = project_id
         self.branch = branch
+        self.action_type = action_type
         self.merge_id = merge_id
         self.api_url = re_gitlab_url.match(self.repo_url).group(0) + 'api/v3'
         self.log = logging.getLogger(self.__class__.__name__)
@@ -145,12 +150,13 @@ class GitLabAPI:
 
     def _prepare_request(self, action):
         """Prepare request to GitLab server, build API endpoint URL."""
-        method, endpoint = action.split('_')
-        url = '{api_url}/projects/{project_id}'
-        url += '/merge_request/{merge_id}/' + endpoint
-        url = url.format(api_url=self.api_url, project_id=self.project_id,
-                         merge_id=self.merge_id)
-        return method.upper(), url
+        if self.action_type == 'merge_request':
+            method, endpoint = action.split('_')
+            url = '{api_url}/projects/{project_id}'
+            url += '/merge_request/{merge_id}/' + endpoint
+            url = url.format(api_url=self.api_url, project_id=self.project_id,
+                             merge_id=self.merge_id)
+            return method.upper(), url
 
     def _make_request(self, action, payload=None):
         """Make a request to GitLab server."""
@@ -247,7 +253,10 @@ class GitLabAPI:
                     'can not clone repo %s into %s <%s>',
                     self.clone_url, self.repo_dir, output)
                 return False
-        self.apply_patch()
+
+        if self.action_type == 'merge_request':
+            self.apply_patch()
+
         return True
 
     def run_test_cmd(self):
@@ -260,6 +269,20 @@ class GitLabAPI:
             self.log.warning(msg)
             self.post_merge_request_comment(msg)
         return ok
+
+    def run_on_push_cmd(self):
+        """Run command on push events."""
+        for targets in conf['run_on_push']:
+            for branch_pattern, cmd in targets.items():
+                found = re.match(branch_pattern + '$', self.branch)
+                if found:
+                    ok, output = run_cmd(cmd)
+                    if not ok:
+                        msg = 'On push command failed, '
+                        msg += 'please checkout output of **%s**:\n' % cmd
+                        msg += '<pre>%s</pre>' % output
+                        self.log.warning(msg)
+                    return ok
 
     def apply_patch(self):
         """Apply patch."""
@@ -288,7 +311,8 @@ def process_merge_request():
             clone_url=item['repository']['url'],
             project_id=item['object_attributes']['target_project_id'],
             branch=item['object_attributes']['target_branch'],
-            merge_id=item['object_attributes']['id']
+            merge_id=item['object_attributes']['id'],
+            action_type='merge_request'
         )
         tests_results = [True]
         if conf['validate_commit_messages']:
@@ -300,23 +324,55 @@ def process_merge_request():
         if not all(tests_results):
             gitlab_api.close_merge_request()
 
+
+def process_push():
+    """Process push events.
+
+    This is blocking operation: we awaiting for a new incoming push events.
+    """
+    item = push_queue.get(block=True)
+    gitlab_api = GitLabAPI(
+        repo_url=item['repository']['homepage'],
+        clone_url=item['repository']['url'],
+        project_id=item['project_id'],
+        branch=item['ref'].split('refs/heads/')[1],
+        merge_id=None,
+        action_type='push'
+    )
+    if conf['process_push']:
+        done = gitlab_api.prepare_workdir()
+        if done:
+            gitlab_api.run_on_push_cmd()
+
+
+def run_webapp():
+    """Run web app."""
+    from wsgiref import simple_server
+
+    httpd = simple_server.make_server(
+        conf['listen_address'], conf['listen_port'], api)
+    httpd.serve_forever()
+
 gitlab_webhook_receiver = GitLabWebHookReceiver()
 api = application = falcon.API(middleware=[
     AuthMiddleware(), RequireJSON(), JSONTranslator()])
 api.add_route('/gitlab/webhook', gitlab_webhook_receiver)
 
+
 if __name__ == '__main__':
 
-    pid = os.fork()
-    if pid:
-        # parent process
-        from wsgiref import simple_server
+    web_app = Process(target=run_webapp)
+    web_app.daemon = True
+    web_app.start()
 
-        httpd = simple_server.make_server(
-            conf['listen_address'], conf['listen_port'], api)
-        httpd.serve_forever()
-    else:
-        # child process
-        while True:
-            process_merge_request()
-            time.sleep(2)
+    if conf['validate_commit_messages'] or conf['run_tests']:
+        on_merge_request = Process(target=process_merge_request)
+        on_merge_request.daemon = True
+        on_merge_request.start()
+
+    if conf['run_on_push']:
+        on_push = Process(target=process_push)
+        on_push.daemon = True
+        on_push.start()
+
+    web_app.join()
